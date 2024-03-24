02ffb258fefea0b1cdacfa1429e7ecd18ea29b79
repath500 <75042922+repath500@users.noreply.github.com>
2024-03-24 01:59:37 +0000

    groq api

--- utils/index.ts
+++ utils/index.ts
@@ -5,10 +5,10 @@ export const OpenAIStream = async (messages: Message[]) => {
   const encoder = new TextEncoder();
   const decoder = new TextDecoder();
 
-  const res = await fetch("https://api.openai.com/v1/chat/completions", {
+  const res = await fetch("https://api.groq.com/openai/v1/chat/completions", {
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`
+      Authorization: `Bearer ${process.env.GROQ_API_KEY}`
     },
     method: "POST",
     body: JSON.stringify({
